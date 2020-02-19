90a2369dce5e508d026c397e87dd265ca98b881c
Infiusier <36018101+Infiusier@users.noreply.github.com>
2020-02-19 17:21:52 -0300

    Update Quicksort.py

--- Quicksort.py
+++ Quicksort.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import time
-
+import sys
+sys.setrecursionlimit(10**9)
 lista=[]
 tamanhos=[100000,200000,300000,400000,500000]
 tempos=[]
 def geraLista(tamanho):
   x=[]
-  while(len(x)<tamanho):
-    j=random.randrange(tamanho)
-    if j not in x:
-      x.append(j)
+  for i in range(tamanho): x.append(i)
+  random.shuffle(x)
   return x
 
 def geraListaPiorCaso(tamanho):
@@ -21,32 +20,39 @@ def geraListaPiorCaso(tamanho):
     x.append(i)
   return x
 
-def particao(lista,primeiro,ultimo): 
-    i = ( primeiro-1 )        
-    pivot = lista[ultimo]      
-    for j in range(primeiro , ultimo): 
-        if   lista[j] <= pivot:
-            i = i+1 
-            lista[i],lista[j] = lista[j],lista[i] 
+def randompivot(lista, primeiro, ultimo):
+    rand = random.randrange(primeiro, ultimo)
+    lista[ultimo - 1], lista[rand] = lista[rand], lista[ultimo - 1]
+    return particao(lista, primeiro, ultimo)
+
+def particao(lista, primeiro, ultimo):
+    pivot = lista[ultimo - 1]
+    for i in range(primeiro, ultimo):
+        if not lista[i] > pivot:
+            lista[primeiro], lista[i] = lista[i], lista[primeiro]
+            primeiro += 1
+    return primeiro-1 
   
-    lista[i+1],lista[ultimo] = lista[ultimo],lista[i+1] 
-    return ( i+1 ) 
-  
-def quicksort(lista,primeiro,ultimo): 
-  if primeiro < ultimo: 
-      pi = particao(lista,primeiro,ultimo) 
-      quickSort(lista, primeiro, pi-1) 
-      quickSort(lista, pi+1, ultimo)
+def quicksort(lista, primeiro, ultimo):
+    if primeiro < ultimo:
+        pivot = randompivot(lista, primeiro, ultimo)
+        quicksort(lista, primeiro, pivot)
+        quicksort(lista, pivot + 1, ultimo)
+    
 
 
 for i in tamanhos:
-  lista=geraLista(i)
-  #lista=geraListaPiorCaso(i)
+  print("comecei")
+  #lista=geraLista(i)
+  
+  lista=geraListaPiorCaso(i)
+  print("terminei")
   now=time.time()
+  print("comecei dnv")
   quicksort(lista,0,i-1)
+  print("acabei dnv")
   then=time.time()
   tempos.append(then-now)
-  #print(lista)
 # Plot the data
 plt.plot(tamanhos,tempos)
 print(tempos)
